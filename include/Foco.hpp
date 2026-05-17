01c2d435a132be72f7b6b61d4e285dcacc161be3
ChristianG25110266 <a25110266@ceti.mx>
2026-05-17 00:22:01 +0000

    Trabajos segundo parcial

---
+++ include/Foco.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <EstadoFoco.hpp>
+class Foco
+{
+private:
+    EstadoFoco encendido;
+
+public:
+    Foco(){
+        encendido = "-";
+    }
+    ~Foco(){}
+    void Encender (){
+        this->encendido = "+";
+    }
+    void Apagar(){
+        this->encendido = "-";
+    }
+    EstadoFoco LeerEstado(){
+        return this->encendido;
+    }
+};
---
+++ include/SerieFocos.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <Foco.hpp>
+
+class SerieFocos
+{
+private:
+    Foco focos[10];
+public :
+    void MostrarFocos(){
+        for (auto &&f : focos){
+            std::cout<<f.LeerEstado();
+        }
+        std::cout << std::endl;
+    }
+    void EncenderFocos(){
+        for (auto &&f : focos){
+            f.Encender();
+        }
+    }
+    void ApagarFocos(){
+        for (auto &&f : focos){
+            f.Apagar();
+        }
+    }
+};
